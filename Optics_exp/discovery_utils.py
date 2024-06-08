a400c934737cb4ad4d2f4599b940571b5e04efa3
khrstln <Khrstln@yandex.ru>
2024-06-08 20:02:52 +0300

    decompose the epde_discovery() method

--- Optics_exp/discovery_utils.py
+++ Optics_exp/discovery_utils.py
@@ -4,7 +4,7 @@ from epde.interface.prepared_tokens import GridTokens, CacheStoredTokens
 from epde.interface.interface import EpdeSearch
 
 
-def set_de_params(epde_search_obj: epde_alg.EpdeSearch, pop_size: int, training_epochs: int):
+def set_de_params(epde_search_obj: epde_alg.EpdeSearch, pop_size: int, training_epochs: int) -> None:
     """
     Set the MOEADD parameters for the EPDE search object.
 
@@ -16,10 +16,11 @@ def set_de_params(epde_search_obj: epde_alg.EpdeSearch, pop_size: int, training_
     Returns:
         None
     """
+
     epde_search_obj.set_moeadd_params(population_size=pop_size, training_epochs=training_epochs)
 
 
-def get_polynomial_family(tensor: np.ndarray, order: int, token_type='polynomials'):
+def get_polynomial_family(tensor: np.ndarray, order: int, token_type='polynomials') -> CacheStoredTokens:
     """
     Get family of tokens for polynomials of orders from second up to the order argument.
 
@@ -42,9 +43,31 @@ def get_polynomial_family(tensor: np.ndarray, order: int, token_type='polynomial
                              params_equality_ranges=None, meaningful=True)
 
 
+def set_epde_preprocessor(epde_search_obj: EpdeSearch, use_ann: bool = False) -> None:
+    """
+    Set the preprocessor for an EpdeSearch object based on the specified configuration.
+
+    Args:
+        epde_search_obj: The EpdeSearch object to set the preprocessor for.
+        use_ann: A boolean indicating whether to use the ANN preprocessor (default is False).
+
+    Returns:
+        None
+    """
+
+    if use_ann:
+        epde_search_obj.set_preprocessor(default_preprocessor_type='ANN',
+                                         preprocessor_kwargs={'epochs_max': 50000})
+    else:
+        epde_search_obj.set_preprocessor(default_preprocessor_type='poly',
+                                         preprocessor_kwargs={'use_smoothing': False, 'sigma': 1,
+                                                              'polynomial_window': 3,
+                                                              'poly_order': 3})
+
+
 def epde_discovery(grid: np.ndarray, poynting_vec: np.ndarray, pop_size: int = 5,
                    factors_max_number: int = 1, poly_order: int = 4, training_epochs: int = 100,
-                   variable_names: [str] = None, max_deriv_order: tuple = (2,),
+                   variable_names: [str] = None, max_deriv_order: (int, ) = (2,),
                    equation_terms_max_number: int = 5, data_fun_pow: int = 1,
                    use_ann: bool = False, derivs: [[np.ndarray]] = None) -> EpdeSearch:
     """
@@ -73,29 +96,20 @@ def epde_discovery(grid: np.ndarray, poynting_vec: np.ndarray, pop_size: int = 5
     dimensionality = poynting_vec.ndim - 1
     epde_search_obj = epde_alg.EpdeSearch(use_solver=False, dimensionality=dimensionality,
                                           coordinate_tensors=[grid, ])
-    if use_ann:
-        epde_search_obj.set_preprocessor(default_preprocessor_type='ANN',
-                                         preprocessor_kwargs={'epochs_max': 50000})
-    else:
-        epde_search_obj.set_preprocessor(default_preprocessor_type='poly',
-                                         preprocessor_kwargs={'use_smoothing': False, 'sigma': 1,
-                                                              'polynomial_window': 3,
-                                                              'poly_order': 3})
+
+    set_epde_preprocessor(epde_search_obj, use_ann=use_ann)
+
     epde_search_obj.set_moeadd_params(population_size=pop_size, training_epochs=training_epochs)
+
     custom_grid_tokens = GridTokens(dimensionality=dimensionality)
     polynomial_tokens = get_polynomial_family(poynting_vec, poly_order)
-    if derivs is None:
-        epde_search_obj.fit(data=[poynting_vec, ], variable_names=variable_names, max_deriv_order=max_deriv_order,
-                            equation_terms_max_number=equation_terms_max_number, data_fun_pow=data_fun_pow,
-                            additional_tokens=[polynomial_tokens, custom_grid_tokens],
-                            equation_factors_max_number=factors_max_number,
-                            eq_sparsity_interval=(1e-12, 1e-4))
-    else:
-        epde_search_obj.fit(data=[poynting_vec, ], variable_names=variable_names, max_deriv_order=max_deriv_order,
-                            derivs=[derivs, ],
-                            equation_terms_max_number=equation_terms_max_number, data_fun_pow=data_fun_pow,
-                            additional_tokens=[polynomial_tokens],
-                            equation_factors_max_number=factors_max_number,
-                            eq_sparsity_interval=(1e-12, 1e-4))
+
+    kwargs = {'data': [poynting_vec, ], 'variable_names': variable_names,
+              'max_deriv_order': max_deriv_order, 'derivs': [derivs, ],
+              'equation_terms_max_number': equation_terms_max_number, 'data_fun_pow': data_fun_pow,
+              'additional_tokens': [polynomial_tokens, custom_grid_tokens if derivs is None else polynomial_tokens],
+              'equation_factors_max_number': factors_max_number, 'eq_sparsity_interval': (1e-12, 1e-4)}
+    epde_search_obj.fit(**kwargs)
+
     epde_search_obj.equations(only_print=True, num=1)
     return epde_search_obj
