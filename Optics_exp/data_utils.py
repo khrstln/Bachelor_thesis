68c668d23e4c6e94f662997bfe0b0359cf09089b
khrstln <Khrstln@yandex.ru>
2024-06-08 20:05:48 +0300

    add info about r0 in the docstring

--- Optics_exp/data_utils.py
+++ Optics_exp/data_utils.py
@@ -1,21 +1,21 @@
-import os
-
+from pathlib import Path
 import numpy as np
 
 
-def read_data(r0: float) -> (np.ndarray, np.ndarray):
+def get_data(r0: int | float) -> (np.ndarray, np.ndarray):
     """
-    Reads grid and Poynting vector data from files based on the fixed r0 value.
+    Reads and gets grid and Poynting vector data from files based on the fixed r0 value.
 
     Args:
-        r0 (float): The fixed r0 value used to locate the data files.
+        r0 (float): The fixed r0 value used to locate the data files. This is the radius of the dielectric
+        inclusions in 2D supercell model of the inhomogeneous layer.
 
     Returns:
         tuple: A tuple containing the grid data and Poynting vector data.
     """
-    data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), fr'data\optics_data/T(H) r0={r0}'))
-    grid_file = os.path.join(data_dir, f'grid_{r0}.txt')
-    poynting_vec_file = os.path.join(data_dir, f'T_av_{r0}.txt')
+    data_dir = Path(__file__) / 'data' / 'optics_data' / f'T(H) r0={r0}'
+    grid_file = data_dir / f'grid_{r0}.txt'
+    poynting_vec_file = data_dir / f'T_av_{r0}.txt'
     grid = np.genfromtxt(grid_file, delimiter=',')
     poynting_vec = np.genfromtxt(poynting_vec_file, delimiter=',')
     return grid, poynting_vec
